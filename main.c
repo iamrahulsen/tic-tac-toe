fda488c7277fce275c19225dd2a8bebc4f0ca3be
Rahul Sen <iamrahulsen100@gmail.com>
2021-05-23 00:25:00 +0530

    final- condition added

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <conio.h>
+#include <unistd.h>
 
 char arr[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}; // arr for the game grid
 char p1_name[100], p2_name[100];                             // player's name
-
+int end = 1;
+int res =0;
 void game_name(char *g_name)
 {
     printf("\n\t\t\t\t\t\t\t\t\t");
-    printf("**Let's Play %s**\n\n\n", g_name); // displays the game name at the top
+    printf("**Let's Play %s**\n\n", g_name); // displays the game name at the top
 }
 
 void show_grid()
 {
     // draws the grid for playing the game with position number
-    printf("\t\t\t\t\t\t\t\t\t      %c  |  %c  |  %c \n", arr[0], arr[1], arr[2]);
+    printf("\n\n\t\t\t\t\t\t\t\t\t      %c  |  %c  |  %c \n", arr[0], arr[1], arr[2]);
     printf("\t\t\t\t\t\t\t\t\t    -----|-----|-----\n");
     printf("\t\t\t\t\t\t\t\t\t      %c  |  %c  |  %c \n", arr[3], arr[4], arr[5]);
     printf("\t\t\t\t\t\t\t\t\t    -----|-----|-----\n");
@@ -28,7 +30,7 @@ void game_symbols()
 void symbol()
 {
     printf("\n\t\t\t\t\t\t\t%s, Your symbol is X \n ", p1_name);
-    printf("\t\t\t\t\t\t\t%s, Your symbol is O \n", p2_name); 
+    printf("\t\t\t\t\t\t\t%s, Your symbol is O \n\n", p2_name); 
 
 }
 
@@ -36,13 +38,14 @@ void start()
 {
     printf("\n\n\t\t\t\t\t\t\t So, Who is going the start the game? Player 1 enter you name: ");
     gets(p1_name);
-    printf("\t\t\t\t\t\t\t **Best of luck %[^\n]", p1_name);
+    printf("\t\t\t\t\t\t\t **Best of luck %s, Your symbol is X \n", p1_name);
 
     printf("\n\t\t\t\t\t\t\t Player 2 enter you name: ");
     gets(p2_name);
-    printf("\t\t\t\t\t\t\t **Best of luck %[^\n]", p2_name);
+    printf("\t\t\t\t\t\t\t **Best of luck %s, Your symbol is X \n", p2_name);
+    sleep(2);
     system("cls");
-    show_grid();
+    game_name("Tic Tac Toe");
     symbol();
 }
 
@@ -52,8 +55,8 @@ void play()
     static int k = 0;
     if (k == 0)
     {
-        printf("\n\t\t\t\t\t\t\t Enter your postion %s: ", p1_name);
-        scanf("%c", &position);
+        printf("\n\t\t\t\t\t\t\t Enter your postion, %s: ", p1_name);
+        scanf(" %c", &position);
         for (int i = 0; i <= 8; i++)
         {
             if (arr[i] == position)
@@ -67,7 +70,7 @@ void play()
     else
     {
         printf("\n\t\t\t\t\t\t\t Enter your postion %s: ", p2_name);
-        scanf("%c", &position);
+        scanf(" %c", &position);
         for (int i = 0; i <= 8; i++)
         {
             if (arr[i] == position )
@@ -80,19 +83,113 @@ void play()
     }
 }
 
-void won()
+int who_won()
 {
+    if(arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X')
+        return 1;
+    else if(arr[0] == 'X' && arr[3] == 'X' && arr[6] == 'X')
+        return 1;
+    else if(arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X')
+        return 1;
+    else if(arr[1] == 'X' && arr[4] == 'X' && arr[7] == 'X')
+        return 1;
+    else if(arr[2] == 'X' && arr[5] == 'X' && arr[8] == 'X')
+        return 1;
+    else if(arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X')
+        return 1;
+    else if(arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X')
+        return 1;
+    else if(arr[6] == 'X' && arr[7] == 'X' && arr[8] == 'X')
+        return 1;
+
+    else if(arr[0] == 'O' && arr[1] == 'O' && arr[2] == 'O')
+        return 2;
+    else if(arr[0] == 'O' && arr[3] == 'O' && arr[6] == 'O')
+        return 2;
+    else if(arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O')
+        return 2;
+    else if(arr[1] == 'O' && arr[4] == 'O' && arr[7] == 'O')
+        return 2;
+    else if(arr[2] == 'O' && arr[5] == 'O' && arr[8] == 'O')
+        return 2;
+    else if(arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O')
+        return 2;
+    else if(arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O')
+        return 2;
+    else if(arr[6] == 'O' && arr[7] == 'O' && arr[8] == 'O')
+        return 2;
+    else 
+        return 3;
+
 }
+
+void result()
+{
+    int draw = 0;
+    res = who_won();
+    if(res == 1)
+    {
+        system("cls");
+        game_name("Tic Tac Toe");
+        show_grid();
+        printf("\n\t\t\t\t\t\t\tCongrats %s!, you won the game.",p1_name);
+        end = 0;
+    }
+    if (res == 2) {
+        system("cls");
+        game_name("Tic Tac Toe");
+        show_grid();
+        printf("\n\t\t\t\t\t\t\tCongrats %s!, you won the game.",p2_name);
+        end = 0;
+    }
+}
+
 
 int main()
 {
+    again:
     system("cls");            // clearing the screen for gcc/g++ compiler in Linux
     game_name("Tic Tac Toe"); // displays the name of the game
     show_grid();              // draws the grid for playing the game
     game_symbols();   // defines the symbols of the game
     start();
-    //play();
-
+    
+    while(end)
+    {
+        system("cls");
+        game_name("Tic Tac Toe");
+        symbol();
+        show_grid();
+        play();
+        result();
+    }
+    if(res == 0)
+    {
+        printf("Oh! tough fight, it's a DRAW....\n");
+    }
+    printf("Do you want to play again? (y/n)");
+    char ch;
+    scanf(" %c,",&ch);
+    if(ch == 'y' || ch == 'Y')
+    {
+        arr[0] = '1';
+        arr[1] = '2';
+        arr[2] = '3';
+        arr[3] = '4';
+        arr[4] = '5';
+        arr[5] = '6';
+        arr[6] = '7';
+        arr[7] = '8';
+        arr[8] = '9';
+        end = 1;
+        res = 0;
+        goto again;
+    }
+    else
+    {
+        printf("Thanks for playing! See you again.");
+    }
+    
     getch();
 
     return 0;
